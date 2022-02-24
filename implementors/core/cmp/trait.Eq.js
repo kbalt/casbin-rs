e7e0632155f88f33af723a39e1d121a48f1bec08
PsiACE <PsiACE@users.noreply.github.com>
2022-02-24 17:56:27 +0000

    deploy: 9320add836f2c66204c082e4b4ecf3b4f79fc015

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["casbin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"casbin/prelude/enum.Event.html\" title=\"enum casbin::prelude::Event\">Event</a>","synthetic":false,"types":["casbin::emitter::Event"]}];
+implementors["casbin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"casbin/prelude/enum.Event.html\" title=\"enum casbin::prelude::Event\">Event</a>","synthetic":false,"types":["casbin::emitter::Event"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
