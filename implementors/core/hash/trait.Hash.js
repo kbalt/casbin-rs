22100f917972581cf2b7f7bcd87600393290dacd
hackerchai <hackerchai@users.noreply.github.com>
2022-05-04 07:12:18 +0000

    deploy: 41db26043186b5d708b5d77857acb2295caa5b86

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["casbin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"casbin/prelude/enum.Event.html\" title=\"enum casbin::prelude::Event\">Event</a>","synthetic":false,"types":["casbin::emitter::Event"]}];
+implementors["casbin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"casbin/prelude/enum.Event.html\" title=\"enum casbin::prelude::Event\">Event</a>","synthetic":false,"types":["casbin::emitter::Event"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
